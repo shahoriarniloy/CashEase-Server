d6883c6cc458af7031f7a880d02274b519b3f4d2
shahoriarniloy <niloyshahoriar@gmail.com>
2024-07-16 20:10:19 +0600

    Jwt Implemented

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+app.use(cookieParser());
+
 
 const port = process.env.PORT || 5000;
 
@@ -33,40 +38,77 @@ const client = new MongoClient(uri, {
 
 
 
+const logger = (req, res, next)=>{
+  console.log('log:info',req.method, req.url);
+  next();
+}
+
+
+
+const verifyToken = (req, res, next)=>
+  {
+    if(!req.headers.authorization){
+      return res.status(401).send({message:'Unauthorized Access'})
+    }
+    const token = req.headers.authorization.split(' ')[1];
+
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){      
+        return res.status(401).send({message:'Unauthorized Access'})
+
+      }
+    req.decoded=decoded;
+    next();
+    })
+  }
+
+
+
+
+
 
 
 async function run() {
   try {
     const database = client.db("mfs");
     const userCollection = database.collection("users");
+
+
+
+    
+    app.post('/jwt', async (req, res) => {
+    const user = req.body;
+     console.log(user);
+     const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+     res.cookie('token',token,{
+      httpOnly:true,
+      secure:false, 
+    })
+     .send({success:true})
+    });
     
 
-    app.post('/register',async (req,res)=>{
-      const user =req.body;
-      
 
-      
+
+    app.post('/register', async (req, res) => {
+      const user = req.body;
+    
       const existingUserByEmail = await userCollection.findOne({ email: user.email });
-      console.log('email',existingUserByEmail);
-
       const existingUserByPhone = await userCollection.findOne({ phone: user.phone });
-      console.log('phone',existingUserByPhone);
-
-      
+    
       if (existingUserByEmail || existingUserByPhone) {
         return res.send({ message: 'User already exists', insertedId: null });
       }
-      
+    
       const result = await userCollection.insertOne(user);
-      res.send(result);
-      
-    })
+      res.json(result); 
+    });
+    
     
 
     app.post('/login', async (req, res) => {
       const { input, pin } = req.body;
 
-      // Find user by email or phone
       const query = { $or: [{ email: input }, { phone: input }] };
       const user = await userCollection.findOne(query);
 
@@ -74,30 +116,51 @@ async function run() {
         return res.status(404).json({ message: 'User not found' });
       }
 
-      // Compare hashed PIN with input PIN
       const isMatch = await bcrypt.compare(pin, user.pin);
 
       if (!isMatch) {
         return res.status(401).json({ message: 'Invalid PIN' });
       }
 
-      // Successful login
       res.status(200).json({ message: 'Login successful', user });
     });
 
+
+
+    app.post('/logout', async (req, res) => {
+      const token = req.cookies.token;
+      if (token) {
+          res.clearCookie('token').json({ success: true, message: 'Logged out successfully' });
+      } else {
+          res.status(400).json({ success: false, message: 'No token found' });
+      }
+  });
+    
     
       
   
 
 
-    app.get('/users',  async(req,res)=>{
-      console.log(req.headers);
-      const users = userCollection.find();
-      const result = await users.toArray();
-      res.send(result);
-    })
-
-   
+    app.get('/userrole', async (req, res) => {
+      const userInput = req.query.input;
+      console.log('user::::',userInput);
+      
+      try {
+        const user = await userCollection.findOne({
+          $or: [{ email: userInput }, { phone: userInput }],
+        });
+  
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+  
+        const { role } = user;
+        res.status(200).json({ role });
+      } catch (error) {
+        console.error('Error finding user:', error.message);
+        res.status(500).json({ message: 'Server error' });
+      }
+    });
 
 
 
@@ -123,7 +186,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('MedicoDirect Server');
+  res.send('CashEase Server');
 });
 
 app.listen(port, () => {
